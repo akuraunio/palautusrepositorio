ab73f38897cafe58ac8fc8283bce476785149177
akuraunio <160453163+akuraunio@users.noreply.github.com>
2025-11-10 15:01:53 +0200

    Tehtävä 5

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,19 +1,33 @@
 from player import Player
 from player_reader import PlayerReader
 from player_stats import PlayerStats
+import requests
+from rich.console import Console
+from rich.table import Table
 
 def main():
-    url = "https://studies.cs.helsinki.fi/nhlstats/2024-25/players"
+    console = Console()
+    season = console.input("Season [magenta][2018-19/2019-20/2020-21/2021-22/2022-23/2023-24/2024-25/2025-26][/] ")
+    nationality = console.input("Nationality [magenta][USA/FIN/CAN/SWE/CZE/RUS/SLO/FRA/GBR/SVK/DEN/NED/AUT/BLR/GER/SUI/NOR/UZB/LAT/AUS][/]")
+
+    url = f"https://studies.cs.helsinki.fi/nhlstats/{season}/players"
     reader = PlayerReader(url)
     stats = PlayerStats(reader)
 
-    country = "FIN"
+    players = stats.top_scorers_by_nationality(nationality)
 
-    players = stats.top_scorers_by_nationality(country)
+    table = Table(title=f"Season {season} players from {nationality}")
 
-    print("Players from FIN \n")
+    table.add_column("Released", style="blue")
+    table.add_column("teams", style="purple")
+    table.add_column("goals", style="green")
+    table.add_column("assists", style="green")
+    table.add_column("points", style="green")
+
     for player in players:
-        print(player)
+        table.add_row(str(player.name), str(player.team), str(player.goals), str(player.assists), str(player.points))
+
+    console.print(table)
 
 
 if __name__ == "__main__":
